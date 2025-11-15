90d9b7e439bb673b9efe02869b3b853fc5a3125b
Amit Wieder <wiederamit@gmail.com>
2025-11-15 12:36:37 -0700

    added SVD decomposition and test with low k value

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pandas as pd
 import re
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.decomposition import TruncatedSVD
 #Load Data
 data = pd.read_csv("data/Articles.csv", encoding="ISO-8859-1")
 #Clean Text:
@@ -19,3 +20,14 @@ vectorizer = CountVectorizer(max_features=2000, stop_words='english')
 X = vectorizer.fit_transform(data['Clean Articles'])
 word_freq_matrix = pd.DataFrame(X.toarray(), columns=vectorizer.get_feature_names_out())
 print(word_freq_matrix)
+#Preform Truncated Decomposition
+def TruncateSVD(k, X):
+    svd = TruncatedSVD(n_components = k)
+    X_svd = svd.fit_transform(X)
+    return X_svd
+
+X_low_k = TruncateSVD(5, X)
+svd_df = pd.DataFrame(X_low_k)
+print(svd_df)
+# X_mid_k = TruncateSVD(50, X)
+# X_high_k = TruncateSVD(100, X)
